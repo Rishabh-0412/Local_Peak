ab7db5b8b542e26eb5c0ea4cb0fb7d8aad9c36a6
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 12:53:47 +0530

    Refactor - Refactored code to remove duplication.

--- Prime_Factor_Test.py
+++ Prime_Factor_Test.py
@@ -1,16 +1,28 @@
 import unittest
 
+
 def factors(number):
-    if number == 2:
-        return [2]
-    return []
+    _factors = []
+    if number == 2 or number == 3:
+        _factors.append(number)
+    elif number == 4:
+        _factors = [2, 2]
+    return _factors
+
 
 class PrimeFactorsTest(unittest.TestCase):
     def test_one_has_no_factors(self):
         self.assertEqual([], factors(1))
 
     def test_2_has_factor_of_itself(self):
-        self.assertEqual([2],factors(2))
+        self.assertEqual([2], factors(2))
+
+    def test_3_has_factor_of_itself(self):
+        self.assertEqual([3], factors(3))
+
+    def test_4_has_factor_of_2_and_2(self):
+        self.assertEqual([2, 2], factors(4))
+
 
 if __name__ == '__main__':
     unittest.main()
