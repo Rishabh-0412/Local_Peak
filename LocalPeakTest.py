e04a7eaa65d50b90525f1d0d3b32c404b25682e0
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 16:54:27 +0530

    Added - case of array with 2 elements!

--- LocalPeakTest.py
+++ LocalPeakTest.py
@@ -2,9 +2,16 @@ import unittest
 
 
 def get_peak(num_list):
-    return num_list
+    if len(num_list) == 2:
+        if num_list[0] > num_list[1]:
+            return num_list[0]
+        return num_list[1]
+    return num_list[0]
 
 
 class LocalPeakTest(unittest.TestCase):
     def test_peak_when_array_has_one_element(self):
-        self.assertEqual([2], get_peak([2]))
+        self.assertEqual(2, get_peak([2]))
+
+    def test_peak_when_array_has_two_element(self):
+        self.assertEqual(10, get_peak([9, 10]))
