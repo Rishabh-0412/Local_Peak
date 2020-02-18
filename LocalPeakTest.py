8df88b280930788d3526834f48a43e43f5a3b556
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 17:06:12 +0530

    Added - case of array with 3 elements with peak in middle and left!

--- LocalPeakTest.py
+++ LocalPeakTest.py
@@ -8,9 +8,9 @@ def get_peak(num_list):
         return num_list[1]
 
     elif len(num_list) == 3:
-        if num_list[0] > num_list[1] and num_list[0] > num_list [2]:
+        if num_list[0] > num_list[1] and num_list[0] > num_list[2]:
             return num_list[0]
-        elif num_list[2] > num_list[1] and num_list[2] > num_list [0]:
+        elif num_list[2] > num_list[1] and num_list[2] > num_list[0]:
             return num_list[2]
         return num_list[1]
 
@@ -22,7 +22,10 @@ class LocalPeakTest(unittest.TestCase):
         self.assertEqual(2, get_peak([2]))
 
     def test_peak_when_array_has_two_element(self):
-        self.assertEqual(10, get_peak([9, 10]))
+        self.assertEqual(9, get_peak([9, 1]))
 
-    def test_peak_when_array_has_three_element(self):
-        self.assertEqual(10, get_peak([9, 10, 7]))
+    def test_peak_array_size_3_and_Peak_at_middle(self):
+        self.assertEqual(10, get_peak([9, 10, 2]))
+
+    def test_peak_array_size_3_and_Peak_at_left(self):
+        self.assertEqual(19, get_peak([19, 10, 2]))
