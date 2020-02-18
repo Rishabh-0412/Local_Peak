fa077f92304b2217a4f1017d935599cafa4b6ef6
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 20:58:09 +0530

    Refactor & Add - All cases of array of lenght 4 and greater are implemented!

--- LocalPeakTest.py
+++ LocalPeakTest.py
@@ -1,47 +1,55 @@
 import unittest
 
 
-def get_peak(num_list):
-    if len(num_list) == 2:
-        if num_list[0] > num_list[1]:
-            return num_list[0]
-        return num_list[1]
+def local_peak_of_list(number):
+    if len(number) == 0:
+        return None
 
-    elif len(num_list) >= 3:
-        if num_list[0] > num_list[1]:
-            return num_list[0]
-        elif num_list[2] > num_list[1]:
-            return num_list[2]
-        elif num_list[1] > num_list[0] and num_list[1] > num_list[2]:
-            return num_list[1]
+    else:
+        if len(number) == 2:
+            if number[0] > number[1]:
+                return number[0]
+            return number[1]
 
-    return num_list[0]
+        elif len(number) >= 3:
+            if number[0] > number[1]:
+                return number[0]
+            return max(number)
+
+        return number[0]
 
 
 class LocalPeakTest(unittest.TestCase):
     def test_peak_when_array_has_one_element(self):
-        self.assertEqual(2, get_peak([2]))
+        self.assertEqual(2, local_peak_of_list([2]))
 
     def test_peak_when_array_has_two_element(self):
-        self.assertEqual(9, get_peak([9, 1]))
+        self.assertEqual(9, local_peak_of_list([9, 1]))
 
     def test_peak_array_size_3_and_Peak_at_middle(self):
-        self.assertEqual(10, get_peak([9, 10, 2]))
+        self.assertEqual(10, local_peak_of_list([9, 10, 2]))
 
     def test_peak_array_size_3_and_Peak_at_left(self):
-        self.assertEqual(19, get_peak([19, 10, 2]))
+        self.assertEqual(19, local_peak_of_list([19, 10, 2]))
 
-    def test_peak_array_size_3_and_Peak_at_left_2(self):
-        self.assertEqual(19, get_peak([19, 10, 22]))
+    def test_peak_array_size_3_and_Peak_at_left_case2(self):
+        self.assertEqual(19, local_peak_of_list([19, 10, 22]))
 
     def test_peak_array_size_3_and_Peak_at_right(self):
-        self.assertEqual(22, get_peak([9, 10, 22]))
+        self.assertEqual(22, local_peak_of_list([9, 10, 22]))
 
     def test_peak_array_size_3_with_all_elements_equal(self):
-        self.assertEqual(19, get_peak([19, 19, 19]))
+        self.assertEqual(19, local_peak_of_list([19, 19, 19]))
 
     def test_peak_array_size_4_and_Peak_at_left(self):
-        self.assertEqual(19, get_peak([19, 10, 22, 89]))
+        self.assertEqual(19, local_peak_of_list([19, 10, 22, 89]))
 
     def test_peak_array_size_4_and_Peak_at_element2(self):
-        self.assertEqual(10, get_peak([1, 10, 2, 99]))
+        self.assertEqual(99, local_peak_of_list([1, 10, 2, 99]))
+
+    def test_peak_null_array(self):
+        self.assertEqual(None, local_peak_of_list([]))
+
+
+if __name__ == '__main__':
+    unittest.main()
