7009263e6aeea1af73f219ba091558e9ca847f35
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 16:48:55 +0530

    Created - Simplest test case and make my code run for that only using list!

--- LocalPeakTest.py
+++ LocalPeakTest.py
@@ -1,10 +1,10 @@
 import unittest
 
 
-def get_peak(value):
-    return value
+def get_peak(num_list):
+    return num_list
 
 
 class LocalPeakTest(unittest.TestCase):
     def test_peak_when_array_has_one_element(self):
-        self.assertEqual(2, get_peak(2))
+        self.assertEqual([2], get_peak([2]))
