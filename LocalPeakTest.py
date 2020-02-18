b38b9c31c5d0472e7e9b2545ed7284d33bdca994
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-18 16:57:09 +0530

    Added - case of array with 3 elements!

--- LocalPeakTest.py
+++ LocalPeakTest.py
@@ -6,6 +6,14 @@ def get_peak(num_list):
         if num_list[0] > num_list[1]:
             return num_list[0]
         return num_list[1]
+
+    elif len(num_list) == 3:
+        if num_list[0] > num_list[1] and num_list[0] > num_list [2]:
+            return num_list[0]
+        elif num_list[2] > num_list[1] and num_list[2] > num_list [0]:
+            return num_list[2]
+        return num_list[1]
+
     return num_list[0]
 
 
@@ -15,3 +23,6 @@ class LocalPeakTest(unittest.TestCase):
 
     def test_peak_when_array_has_two_element(self):
         self.assertEqual(10, get_peak([9, 10]))
+
+    def test_peak_when_array_has_three_element(self):
+        self.assertEqual(10, get_peak([9, 10, 7]))
